0f886bda43b8807bdb678dadc5e4a9ddae8cfb56
Pnttrrv <ndhkptr@gmail.com>
2026-01-10 23:17:08 +0700

    styling product details

--- script.js
+++ script.js
@@ -8,13 +8,34 @@ function getData() {
         const card = document.createElement("div"); //membuat element div
         const productList = document.getElementById("productList");
         card.className = "product"; // <== memberikan class pada div
-        card.style.backgroundImage = `url(${item.image})`;
+        card.style.backgroundImage = `url(${item.image})`; //<== menambahkan background image
+        console.log(item);
         card.innerHTML = `
-        <h1>${item.title}</h1>
-        <p>${item.description}</p>
-        <p>${item.detail_description}</p>
-        <p>${item.price}</p>
-        `; // <== emberikan isi pada div dengan class product
+        <div class="product-details">
+          <h1>${item.title}</h1>
+          <p>${item.subtitle}</p>
+          <div class="product-detail-container">
+            <div class="product-detail">
+              <span class="label">Color</span>
+              <span class="value">${item.color}</span>
+            </div>
+            <div class="product-detail">
+              <span class="label">Battery</span>
+              <span class="value">${item.battery}</span>
+            </div>
+            <div class="product-detail">
+              <span class="label">Weight</span>
+              <span class="value">${item.weight}</span>
+            </div>
+            <div class="product-detail">
+              <span class="label">Latency</span>
+              <span class="value">${item.latency}</span>
+            </div>
+          </div>
+          <h3 class="product-price">${item.price}</h3>
+          
+        </div>
+        `; // <== memberikan isi pada div dengan class product
 
         productList.appendChild(card); // <== mengirim isian yang ditambahkan dengan innerHTML ke index.html
       });
